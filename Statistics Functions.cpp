6f4e74b7320f2d3e08e43bafd133f070553d8cee
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-04-30 18:26:25 -0400

    Initial commit

---
+++ Statistics Functions.cpp
@@ -0,0 +1,55 @@
+//
+//  Statistics Functions.cpp
+//  Statistics Calculator
+//
+//  Created by Joseph Lyons on 4/25/18.
+//  Copyright © 2018 Joseph Lyons. All rights reserved.
+//
+
+#include <math.h>
+#include <vector>
+#include "Statistics Functions.hpp"
+
+double calculateSampleMean (const std::vector<int> &sample)
+{
+    double total = 0;
+    
+    for (int i = 0; i < sample.size(); i++)
+        total += sample[i];
+    
+    return total / sample.size();
+}
+
+double calculateSampleStandardDeviation (const std::vector<int> &sample)
+{
+    return sqrt (calculateSampleVariance (sample));
+}
+
+double calculateSampleVariance (const std::vector<int> &sample)
+{
+    double sampleVariance = 0;
+    double sampleMean = calculateSampleMean (sample);
+    
+    for (int i = 0; i < sample.size(); i++)
+        sampleVariance += pow ((sample[i] - sampleMean), 2);
+    
+    sampleVariance = sampleVariance / (double) (sample.size() - 1);
+    
+    return sampleVariance;
+}
+
+double calculateSampleMean (const int sample[], const int &size)
+{
+    return calculateSampleMean (std::vector<int> (sample, sample + size));
+}
+
+double calculateSampleStandardDeviation (const int sample[], const int &size)
+{
+    return calculateSampleStandardDeviation (std::vector<int> (sample, sample + size));
+}
+
+double calculateSampleVariance (const int sample[], const int &size)
+{
+    return calculateSampleVariance (std::vector<int> (sample, sample + size));
+}
+
---
+++ Statistics Functions.hpp
@@ -0,0 +1,25 @@
+//
+//  Statistics Functions.hpp
+//  Statistics Calculator
+//
+//  Created by Joseph Lyons on 4/25/18.
+//  Copyright © 2018 Joseph Lyons. All rights reserved.
+//
+
+#ifndef Statistics_Functions_hpp
+#define Statistics_Functions_hpp
+
+// For vectors
+double calculateSampleMean (const std::vector<int> &sample);
+double calculateSampleStandardDeviation (const std::vector<int> &sample);
+double calculateSampleVariance (const std::vector<int> &sample);
+
+// For arrays
+double calculateSampleMean (const int sample[], const int &size);
+double calculateSampleStandardDeviation (const int sample[], const int &size);
+double calculateSampleVariance (const int sample[], const int &size);
+
+// Controlling error
+//double controlError
+
+#endif /* Statistics_Functions_hpp */
